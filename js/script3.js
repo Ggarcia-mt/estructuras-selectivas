da634e53e5eb27012d97867832f175bab698b856
Ggarcia-mt <santimartinezdbz1103@gmail.com>
2023-04-19 16:22:29 -0500

    3ser comentario

--- js/script3.js
+++ js/script3.js
@@ -1,1 +1,14 @@
-let nombre,salariob,deducciones,bonos
+let nombre,nombre2,salariob,salariob2,deducciones,deducciones2,bonos,bonos2,t,t2
+nombre=prompt('ingrese nombre 1')
+nombre2=prompt('ingrese nombre 2')
+salariob=Number(prompt('ingrese su salario'))
+salariob2=Number(prompt('ingrese su salario'))
+deducciones=Number(prompt('deducciones'))
+deducciones2=Number(prompt('deducciones2'))
+bonos=Number(prompt('bonos'))
+bonos2=Number(prompt('bonos2'))
+t=salariob+deducciones+bonos
+t2=salariob2+deducciones2+bonos2
+if(t<t2){
+    prompt('resultado'+ )
+}
